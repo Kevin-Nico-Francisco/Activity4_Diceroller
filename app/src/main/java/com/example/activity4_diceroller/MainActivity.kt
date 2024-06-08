860994e265298cdcc7625dd66a77b045f1b88781
Francisco_KevinNico_BSIT32E2 <161100540+Francisco-KevinNico-BSTI32E2@users.noreply.github.com>
2024-06-08 14:58:50 +0800

    Added the images

--- app/src/main/java/com/example/activity4_diceroller/MainActivity.kt
+++ app/src/main/java/com/example/activity4_diceroller/MainActivity.kt
@@ -3,44 +3,76 @@ package com.example.activity4_diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.activity4_diceroller.ui.theme.Activity4_DicerollerTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Activity4_DicerollerTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DiceRollerApp()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Activity4_DicerollerTheme {
-        Greeting("Android")
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableStateOf(1) }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = getDiceImage(result)),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { result = rollDice() }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
+
+fun rollDice(): Int {
+    return Random.nextInt(1, 7)
+}
+
+fun getDiceImage(result: Int): Int {
+    return when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
     }
 }
